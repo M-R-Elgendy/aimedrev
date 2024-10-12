d83b467fa764b512b5e762895f987719505854dc
M-Elgendy <medp.elgendy@gmail.com>
2024-10-13 00:46:48 +0300

    Update file upload module

--- src/file-upload/file-upload.service.ts
+++ src/file-upload/file-upload.service.ts
@@ -8,17 +8,18 @@ export class FileUploadService {
 
   constructor(private configService: ConfigService) {
     this.s3 = new S3({
-      accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
-      region: this.configService.get<string>('AWS_REGION'),
+      accessKeyId: this.configService.getOrThrow<string>('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: this.configService.getOrThrow<string>('AWS_SECRET_ACCESS_KEY'),
+      region: this.configService.getOrThrow<string>('AWS_REGION'),
     });
   }
 
+
   async uploadFile(file: Express.Multer.File): Promise<S3.ManagedUpload.SendData> {
     const params = {
-      Bucket: this.configService.get<string>('AWS_BUCKET_NAME'),
-      Key: file.originalname, // You can customize the key as needed
-      Body: file.buffer, // Use buffer from the file
+      Bucket: this.configService.getOrThrow<string>('AWS_BUCKET_NAME'),
+      Key: file.originalname,
+      Body: file.buffer,
       ContentType: file.mimetype,
     };
 
