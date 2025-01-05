29a8c002d03317cd57117a3108a91aa7db8b42f8
Mohamed Elgendy <54114536+M-R-Elgendy@users.noreply.github.com>
2025-01-05 23:54:18 +0200

    Chnage password roles

--- src/auth/dto/email-signup.dto.ts
+++ src/auth/dto/email-signup.dto.ts
@@ -20,6 +20,6 @@ export class EmailSignUpDto {
     email: string;
 
     @IsString()
-    @IsStrongPassword({ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1 })
+    @IsStrongPassword({ minLength: 8, minNumbers: 1 })
     password: string;
 }
